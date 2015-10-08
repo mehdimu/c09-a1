741ea47e0ba57dd504503b6d4042af6b55510fa2
arshadas <asra.arshad@mail.utoronto.ca>
2015-10-08 19:49:02 -0400

    adding movies.js tutorial code

--- public/js/views/movies.js
+++ public/js/views/movies.js
@@ -6,12 +6,21 @@ var splat =  splat || {};
 
 // note View-name (About) matches name of template file About.html
 splat.MovieThumb = Backbone.View.extend({
-
+	
+    initialize: function() {
+        this.movieThumbLoad = $.get('tpl/MovieThumb.html');
+    },
     // render the View
     render: function () {
-	// set the view element ($el) HTML content using its template
-	this.$el.html(this.template());
-	return this;    // support method chaining
+		this.movieThumbLoad.done(function(markup)) {
+		//idk
+		// Now "markup" contains the response to the $.get() request.
+		// Turn this markup into a function using Underscore's
+		// template() // function.
+		// Finally apply the moviesTemplate shown below to your
+		// movies collection and the template function you just created.
+		this.$el.html(this.template());
+		return this;    // support method chaining
     }
 
 });
