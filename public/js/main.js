a20db088477663c72307326ac9204e49eb7069dc
arshadas <asra.arshad@mail.utoronto.ca>
2015-10-08 18:44:38 -0400

    Adding form and header button

--- public/js/main.js
+++ public/js/main.js
@@ -70,6 +70,7 @@ splat.AppRouter = Backbone.Router.extend({
         };
     // insert the rendered Home view element into the document DOM
         $('#content').html(this.detailsView.render().el);
+		selectMenuItem($('.add-menu')); 
     }
 
 
