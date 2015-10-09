8a4d8bdf9fc218aba690f045c8e128ccb681ec42
arshadas <asra.arshad@mail.utoronto.ca>
2015-10-09 19:45:15 -0400

    bzz

--- public/js/views/movies.js
+++ public/js/views/movies.js
@@ -6,7 +6,6 @@ var splat =  splat || {};
 
 // note View-name (About) matches name of template file About.html
 splat.MovieView = Backbone.View.extend({
-	
     initialize: function() {
         this.movieThumbLoad = $.get('tpl/MovieThumb.html');
     },
@@ -18,23 +17,20 @@ splat.MovieView = Backbone.View.extend({
                         ].join('')),
     // render the View
     render: function () {
-		this.movieThumbLoad.done(function(markup) {
-		// Now "markup" contains the response to the $.get() request.
-		// Turn this markup into a function using Underscore's
-		// template() // function.
-		// Finally apply the moviesTemplate shown below to your
-		// movies collection and the template function you just created.
-        //		console.log(markup);
-		this.template = _.template(splat.thumbMarkup);
-		var someMarkup = this.moviesTemplate({
-						movies: this.collection,
-						movieTemplate: this.template})
-		this.$el.append(someMarkup);
-        console.log(this.movieThumbLoad);
-		return this;
-                //return markup;
-        //                this.$el.html(this.moviesTemplate());
-        //		return this;    // support method chaining
-        })
 		
+		this.movieThumbLoad.done(function(markup) {
+			this.template = _.template(markup);
+			// Now "markup" contains the response to the $.get() request.
+			// Turn this markup into a function using Underscore's
+			// template() // function.
+			// Finally apply the moviesTemplate shown below to your
+			// movies collection and the template function you just created.
+			
+			var someMarkup = this.moviesTemplate({
+							movies: this.collection,
+							movieTemplate: this.template})
+			this.$el.append(someMarkup);
+			console.log(this.movieThumbLoad);
+			return this;
+        })
 }});
